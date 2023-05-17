6b71e58437c24cb6c23aa69e38041e95cf260916
mrmitcp1 <mrmitcp55@gmail.com>
2023-05-17 14:41:30 +0700

    routing

--- as.js
+++ as.js
@@ -1,28 +1,31 @@
-let http= require('http')
+let http = require('http')
 let url = require('url')
-let StringDecoder= require('string_decoder').StringDecoder;
-let handlers ={}
+let StringDecoder = require('string_decoder').StringDecoder;
+let handlers = {}
 let server = http.createServer(function (req, res) {
 
-    let parseUrl = url.parse(req.url,true);
+    let parseUrl = url.parse(req.url, true);
     let path = parseUrl.pathname;
+
     let trimPath = path.replace(/^\/+|\/+$/g, '');
-    req.on('data',function (data) {
+    console.log( router[trimPath])
+    req.on('data', function (data) {
+
     })
-    req.on('end',function (end) {
-        let chosenHandler = (typeof (router[trimPath])!== 'undefined'? router[trimPath] : handlers.notFound)
-        let  data =
+    req.on('end', function (end) {
+        let chosenHandler = (typeof (router[trimPath]) !== 'undefined' ? router[trimPath] : handlers.notFound)
+        let data =
             {
                 'trimPath': trimPath
             }
         ;
-        chosenHandler(data,function (statusCode,payload){
-            statusCode = typeof  (statusCode)==='number'?statusCode:200;
-            payload=typeof (payload)==='object'?payload:{};
+        chosenHandler(data, function (statusCode, payload) {
+            statusCode = typeof (statusCode) === 'number' ? statusCode : 200;
+            payload = typeof (payload) === 'object' ? payload : {};
             let payLoadString = JSON.stringify(payload);
             res.writeHead(statusCode)
             res.end(payLoadString)
-            console.log('status '+statusCode + 'payload' + payload)
+            console.log('status ' + statusCode + 'payload' + payload)
         })
 
     })
@@ -31,19 +34,21 @@ let server = http.createServer(function (req, res) {
 
 })
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('server running')
 })
-handlers.sample=function (data, callback){
-    callback(406,{'name':'sample handle'})
+handlers.sample = function (data, callback) {
+    callback(406, {'name': 'sample handle'})
 };
-handlers.notFound=function (data, callback){
+handlers.notFound = function (data, callback) {
     callback(404);
 };
-handlers.home= function (data, callback){
+handlers.home = function (data, callback) {
     callback(200, 'home page')
 }
-let  router = {
-    'sample':handlers.sample,
-    'home':handlers.home,
+
+
+let router = {
+    'sample': handlers.sample,
+    'home': handlers.home,
 }
